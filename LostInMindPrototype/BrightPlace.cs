a2a7fd25c8f6b5538c187a487b51ceeaaa1a718c
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-21 19:36:28 +0200

    Add: 2 points of interest

---
+++ LostInMindPrototype/BrightPlace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostInMindPrototype
+{
+    class BrightPlace : PointOfInterest
+    {
+        public BrightPlace() 
+        {
+            Name = "Bright place";
+            Description = "As you aproach closer to the light, you realized that this is just a wall with a few torches embedded in it.\n";
+            DescriptionWithItemTaken = "You already took a torch, there is nothing more for you to here.";
+        }
+    }
+}
---
+++ LostInMindPrototype/StartingPlace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LostInMindPrototype
+{
+    class StartingPlace : PointOfInterest
+    {
+        public StartingPlace()
+        {
+            Name = "Starting place";
+            Description = "You see only ground and a chest.\n";
+            DescriptionWithItemTaken = "You see an open chest, there is nothing more to do here.\n";
+        }
+        public bool IsChestOpen { get; private set; }
+
+    }
+}
